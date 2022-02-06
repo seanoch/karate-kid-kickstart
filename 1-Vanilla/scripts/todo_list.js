5dbe67c2468d85a41b73ccda8b3e18846b007a1f
Sean Ocheri <seano@wix.com>
2022-02-06 16:08:58 +0200

    Supporting enter key for input

--- 1-Vanilla/scripts/todo_list.js
+++ 1-Vanilla/scripts/todo_list.js
@@ -1,8 +1,8 @@
 (function () {
-
   let nextId = 0;
-  const iconMap = { "edit": "&#xe3c9", "remove": "&#xe14c;", "check": "&#xe876;"}
+  const iconMap = { edit: "&#xe3c9", remove: "&#xe14c;", check: "&#xe876;" };
   const newItemInput = document.getElementById("new-item-name");
+  const addBtn = document.getElementById("add-btn");
 
   function createButton(id, type, cb) {
     const button = document.createElement("div");
@@ -35,40 +35,42 @@
     newItemLabel.classList.add("item");
     newItemLabel.id = "item-label-" + id;
     newItemLabel.innerText = text;
+    newItemLabel.ondblclick = editItemCallback(id);
     newItemContainer.appendChild(newItemLabel);
 
     const newItemEditBtn = createButton(id, "edit", editItemCallback(id));
     newItemContainer.appendChild(newItemEditBtn);
 
-    const newItemRemoveBtn = createButton(
-      id,
-      "remove",
-      removeItemCallback(id)
-    );
+    const newItemRemoveBtn = createButton(id, "remove", removeItemCallback(id));
     newItemContainer.appendChild(newItemRemoveBtn);
 
     return newItemContainer;
   }
 
-  function addItem() {
-    if (isVaildItemName(newItemInput.value)) {
-      const newItem = createTodoItem(nextId, newItemInput.value);
+  function addItem(event) {
+    if (
+      event.type === "click" ||
+      (event.type === "keyup" && event.keyCode === 13)
+    ) {
+      if (isVaildItemName(newItemInput.value)) {
+        const newItem = createTodoItem(nextId, newItemInput.value);
 
-      nextId++;
+        nextId++;
 
-      document.getElementById("main-container").appendChild(newItem);
-      newItemInput.value = "";
+        document.getElementById("main-container").appendChild(newItem);
+        newItemInput.value = "";
+      }
     }
   }
 
   function isVaildItemName(name) {
-      let valid = name.length > 0;
+    let valid = name.length > 0;
 
-      if (!valid) {
-        alert("Invalid item name!");
-      }
+    if (!valid) {
+      alert("Invalid item name!");
+    }
 
-      return valid;
+    return valid;
   }
 
   function removeItemCallback(id) {
@@ -93,20 +95,26 @@
   }
 
   function confirmItemEditCallback(id) {
-    return function () {
-      const bar = document.getElementById("item-bar-" + id);
-      const input = document.getElementById("item-edit-" + id);
-      const confirmBtn = document.getElementById("confirm-btn-" + id);
+    return function (event) {
+      if (
+        event.type === "click" ||
+        (event.type === "keyup" && event.keyCode === 13)
+      ) {
+        const bar = document.getElementById("item-bar-" + id);
+        const input = document.getElementById("item-edit-" + id);
+        const confirmBtn = document.getElementById("confirm-btn-" + id);
 
-      if (isVaildItemName(input.value)) {
-        const newTextLabel = document.createElement("label");
-        newTextLabel.classList.add("item");
-        newTextLabel.id = "item-label-" + id;
-        newTextLabel.innerText = input.value;
-        bar.replaceChild(newTextLabel, input);
-  
-        const editBtn = createButton(id, "edit", editItemCallback(id));
-        bar.replaceChild(editBtn, confirmBtn);
+        if (isVaildItemName(input.value)) {
+          const newTextLabel = document.createElement("label");
+          newTextLabel.classList.add("item");
+          newTextLabel.id = "item-label-" + id;
+          newTextLabel.innerText = input.value;
+          newTextLabel.ondblclick = editItemCallback(id);
+          bar.replaceChild(newTextLabel, input);
+
+          const editBtn = createButton(id, "edit", editItemCallback(id));
+          bar.replaceChild(editBtn, confirmBtn);
+        }
       }
     };
   }
@@ -123,7 +131,9 @@
       newTextInput.classList.add("edit");
       newTextInput.id = "item-edit-" + id;
       newTextInput.value = oldText;
+      newTextInput.onkeyup = confirmItemEditCallback(id);
       bar.replaceChild(newTextInput, label);
+      newTextInput.focus();
 
       const confirmBtn = createButton(
         id,
@@ -134,5 +144,6 @@
     };
   }
 
-  document.getElementById("add-btn").onclick = addItem;
+  addBtn.onclick = addItem;
+  newItemInput.onkeyup = addItem;
 })();
