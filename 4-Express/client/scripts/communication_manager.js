2b09674c801c98c566ee8b134e92446c3286e646
Sean Ocheri <seano@wix.com>
2022-02-14 20:01:02 +0200

    Initiated server

---
+++ 4-Express/client/scripts/communication_manager.js
@@ -0,0 +1,40 @@
+const axios = require("axios").default;
+const SERVER_URL = "http://localhost:3000";
+
+function addOrUpdateItem(id, item) {
+  const urlWithId = `${SERVER_URL}/${id}`;
+
+  axios.post(urlWithId, item).then(
+    (response) => {
+      console.log(response);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+function removeItem(id) {
+  const urlWithId = `${SERVER_URL}/${id}`;
+
+  axios.delete(urlWithId, { params: { id: id } }).then(
+    (response) => {
+      console.log(response);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+async function getItems() {
+  try {
+    const response = await axios.get(SERVER_URL);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+}
+
+export { addOrUpdateItem, removeItem, getItems };
---
+++ 4-Express/server/mychecklist_server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+const items = {};
+
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(items);
+})
+
+app.post('/:id', (req, res) => {
+  items[req.params.id] = {
+    text:  req.body.text,
+    check: req.body.check
+  };
+  res.send(items);
+})
+
+app.delete('/:id', (req, res) => {
+  delete items[req.params.id]
+  res.send(items);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
